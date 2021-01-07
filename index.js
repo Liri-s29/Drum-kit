2c0aa1738cf55554e1a4a77192be23f888d6b945
Srihari S <75246431+Liri-s29@users.noreply.github.com>
2021-01-07 23:05:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ function addAnimation(currentLetter){
 }
 if(innerWidth < 991){
   var text = $("h3");
-  text.innerHTML="Click on the Buttons (or) Click on the text Box ,and type the respective keys on keyboard to play your Drum Kit.";
-  text.append("<h3 id='title2' > Text Box : </label>");
+  text.innerHTML="Click on the Buttons to play your Drum Kit.";
+  
 }
 
 
